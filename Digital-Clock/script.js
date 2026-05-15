5486db4605896fc82d74955f282267c3624caea1
akbarspace <ahamadbasheera36@outlook.com>
2026-05-15 19:08:05 +0530

    Update script.js

--- Digital-Clock/script.js
+++ Digital-Clock/script.js
@@ -1,12 +1,14 @@
-let hrs = document.getElementById("hrs");
-let mins = document.getElementById("mins");
-let sec = document.getElementById("sec");
+const hrs = document.getElementById("hrs");
+const mins = document.getElementById("mins");
+const sec = document.getElementById("sec");
 
-setInterval(() => {
-    let currentTime = new Date();
+function updateClock() {
+    const now = new Date();
+    
+    hrs.innerHTML = (now.getHours() < 10 ? "0" : "") + now.getHours();
+    mins.innerHTML = (now.getMinutes() < 10 ? "0" : "") + now.getMinutes();
+    sec.innerHTML = (now.getSeconds() < 10 ? "0" : "") + now.getSeconds();
+}
 
-    // நேரத்தை எடுத்து HTML-ல் அப்டேட் செய்கிறது
-    hrs.innerHTML = (currentTime.getHours() < 10 ? "0" : "") + currentTime.getHours();
-    mins.innerHTML = (currentTime.getMinutes() < 10 ? "0" : "") + currentTime.getMinutes();
-    sec.innerHTML = (currentTime.getSeconds() < 10 ? "0" : "") + currentTime.getSeconds();
-}, 1000);
+setInterval(updateClock, 1000);
+updateClock();
