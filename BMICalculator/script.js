8831034bf8e40c5ee721cc4dd911671cb573d15b
akbarspace <ahamadbasheera36@outlook.com>
2026-06-12 11:17:02 +0530

    Update script.js

--- BMICalculator/script.js
+++ BMICalculator/script.js
@@ -1,41 +1,50 @@
-function calculateBMI(){
-    let heightinput=document.getElementById("height").value;
-    let weightinput=document.getElementById("weight").value;
+function calculateBMI() {
+    let heightinput = document.getElementById("height").value;
+    let weightinput = document.getElementById("weight").value;
 
     let weight = parseFloat(weightinput);
     let heightcm = parseFloat(heightinput);
 
-    let height = heightcm/100;
+    let height = heightcm / 100;
 
-    let BMI = weight / (height*height);
-
-    document.getElementById("result").innerHTML=`Your BMI is:${BMI.toFixed(2)}`
+    let BMI = weight / (height * height);
 
     let healthStatus = document.getElementById("BMIlevel");
 
-if (BMI<18.5){
-    healthStatus.innerHTML = "You are Under weight Eat more";
+    if (heightinput <= 0 || weightinput <= 0) {
+        document.getElementById("result").innerHTML = "Plese Enter Your Height and Weight..!";
+        return;
+    }
+    else {
+        document.getElementById("result").innerHTML = `Your BMI is:${BMI.toFixed(2)}`;
+    }
+
+
+    if (BMI < 18.5) {
+        healthStatus.innerHTML = "You are Under weight Eat more";
+    }
+
+    else if (BMI >= 18.5 && BMI <= 24.9) {
+        healthStatus.innerHTML = "You are Healthyyy..💪"
+    }
+
+    else {
+        healthStatus.innerHTML = "You are Over Weight..";
+    }
 }
 
-else if(BMI>=18.5 && BMI<=24.9){
-    healthStatus.innerHTML = "You are Healthyyy..💪"
-}
 
-else{
-    healthStatus.innerHTML = "You are Over Weight..";
-}
-}
 
 let weightBox = document.getElementById("weight");
 let heightBox = document.getElementById("height");
 
- weightBox.addEventListener("keydown", function(event) {
+weightBox.addEventListener("keydown", function (event) {
     if (event.key === "Enter") {
         calculateBMI();
     }
 });
 
- heightBox.addEventListener("keydown", function(event) {
+heightBox.addEventListener("keydown", function (event) {
     if (event.key === "Enter") {
         calculateBMI();
     }
